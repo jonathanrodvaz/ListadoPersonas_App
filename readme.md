10103ae9a70d2ca87a51b54302a7d4b32357f3ab
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-09-11 19:23:30 +0200

    ListadoPersonasV1

---
+++ readme.md
@@ -0,0 +1,1 @@
+Proyecto "Listado de Personas" desarrollado en Java. Segunda lección del curso de Udemy "Java en 13 días con aplicaciones del mundo real" por GM Technologies.
--- src/ListadoPersonasApp.java
+++ src/ListadoPersonasApp.java
@@ -11,6 +11,11 @@ public class ListadoPersonasApp {
         var salir = false;
         while(!salir){
             mostrarMenu();
+            try {
+                salir = ejecutarOperacion(consola, personas);
+            } catch(Exception e) {
+                System.out.println("Ocurrio un error: " + e.getMessage());
+            }
             System.out.println();
         }
         }
@@ -23,6 +28,41 @@ public class ListadoPersonasApp {
                 3. Salir
                 """);
         System.out.print("Proporciona la opcion: ");
+    }
+
+    private static boolean ejecutarOperacion(Scanner consola, List<Persona> personas){
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        // Revisamos la opcion proporcionada
+        switch (opcion){
+            case 1 -> { //Agregar persona a la lista
+                System.out.print("Proporciona el nombre: ");
+                var nombre = consola.nextLine();
+                System.out.print("Proporciona el telefono: ");
+                var tel = consola.nextLine();
+                System.out.print("Proporciona el email: ");
+                var email = consola.nextLine();
+                // Crear el objeto Persona
+                var persona = new Persona(nombre, tel, email);
+                // Lo agregamos a la lista
+                personas.add(persona);
+                System.out.println("La lista tiene: " + personas.size() + " elementos.");
+            } //Fin case 1
+         case 2 -> { //Lista personas
+             System.out.println("Listado de personas: ");
+             //Mejora usando lambda y metodo de refrencia
+             // version alternativa de como hacer esto: personas.forEach((persona) -> System.out.println(persona));
+             personas.forEach(System.out::println);
+            } // Fin case 3
+             case 3 -> { //Salimos de ciclo
+                 System.out.println("Hasta pronto!");
+                 salir = true;
+            }
+            default -> System.out.println("Opcion erronea: " + opcion);
+
+        } //Fin Switch
+
+        return salir;
     }
 
 }
